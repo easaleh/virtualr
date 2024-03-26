fc02a06945f671d8f7b69a0b02622f275aeab9df
Ebrahim Saleh <ebrahimsaleh@ebrahims-air.home>
2024-03-26 11:42:00 +0000

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import FeatureSection from './components/FeatureSection'
+import Footer from './components/Footer'
+import Hero from './components/Hero'
+import Navbar from './components/Navbar'
+import Pricing from './components/Pricing'
+import Testimonials from './components/Testimonials'
+import Workflow from './components/Workflow'
+
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+      <div className="max-w-7xl mx-auto pt-20 px-6">
+        <Hero />
+        <FeatureSection />
+        <Workflow />
+        <Pricing />
+        <Testimonials />
+        <Footer />
+      </div>
+    </div>
+  )
+}
+
+export default App
